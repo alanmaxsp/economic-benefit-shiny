928bcd71098e0f33e6fb02ce466f87f1d281dfc3
alanmaxsp <81830974+alanmaxsp@users.noreply.github.com>
2025-12-18 13:09:16 -0300

    revision dec25

--- app.R
+++ app.R
@@ -18,11 +18,10 @@ ui <- fluidPage(
       numericInput("c","Number of breeding ewes in studs (c)",NULL),
       numericInput("b1","Rams produced per ewe and year in T1 (b1)",NULL),
       numericInput("b2","Rams produced per ewe and year in T2 (b2)",NULL),
-      numericInput("L","Annual survival of rams (s)",NULL,step=0.01),
       numericInput("f","Mating ratio - ewes per ram (f)",NULL),
       numericInput("p","Lambs weaned per ewe mated in T2 and T3 (p)",NULL,step=0.01),
-      numericInput("m1","Survival between weaning and first shearing (s1)",NULL,step=0.01),
-      numericInput("m2","Annual survival after first shearing (s2)",NULL,step=0.01),
+      numericInput("m2","Annual survival of ewes after first shearing (s1)",NULL,step=0.01),
+      numericInput("m1","Annual survival of rams after first mating (s2)",NULL,step=0.01),
       numericInput("u1","Maximum number of matings of ewes (u1)",NULL,min=2,max=8),
       numericInput("u2","Maximum number of matings of rams (u2)",NULL,min=2,max=6),
       numericInput("ecd","Annual genetic progress in WW (sd)",NULL),
@@ -62,37 +61,37 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   datos <- reactiveVal(NULL)
   
-  calcular <- function(c, b1, b2, L, f, p, m1, m2, u1, u2,
+  calcular <- function(c, b1, b2, f, p, m1, m2, u1, u2,
                        ecd, eca, ecm, ece,
                        costoi, costoa, ram1, ram2, r, T, H) {
     #T <- 10; H <- 20; 
     jmax <- 5
     
-    # ---- Fase 1: Números base ----
+    # ---- initial values ----
     carE1 <- rep(0, T); progE2 <- rep(0, T)
     for(k in 1:u2) {
-      carE1[k] <- c * b1 * L^(k-1)
+      carE1[k] <- c * b1 * m1^(k-1)
       progE2[k] <- carE1[k] * f * p
     }
     ncarE1 <- sum(carE1)
     noveE2 <- ncarE1 * f
     carE2 <- rep(0, T); progE3 <- rep(0, T)
     for(k in 1:u2) {
-      carE2[k] <- noveE2 * b2 * L^(k-1)
+      carE2[k] <- noveE2 * b2 * m1^(k-1)
       progE3[k] <- carE2[k] * f * p
     }
     
-    # ---- Probabilidades por edad ----
+    # ---- Probabilities by age ----
     tt <- rep(0, H)
     acum <- sum(m2^(0:(u1-1)))
     tt[1] <- 1/acum
     if(u1 > 1) for(i in 2:u1) tt[i] <- tt[i-1]*m2
-    wm <- rep(0, H); hc <- tt[1]/p
-    for(i in 3:(u1+2)) wm[i] <- hc * tt[i-2]
+    wm <- rep(0, H); wm[3] <- tt[1]/p # modified 6/12/25
+    for(i in 4:(u1+2)) wm[i] <- wm[i-1] * m2 # modified 6/12/25
     wa <- wm; if(3 <= H) wa[2] <- wa[3]/m2
     wf <- rep(0, H); if(u1+2 <= H) wf[u1+2] <- wm[u1+2]
     
-    # ---- Matrices de expresiones ----
+    # ---- Expression matrices ----
     d <- matrix(0,H,jmax); m_mat <- matrix(0,H,jmax)
     a_mat <- matrix(0,H,jmax); e_mat <- matrix(0,H,jmax)
     d[1,1] <- 0.5
@@ -103,36 +102,36 @@ server <- function(input, output, session) {
         temp <- 0
         for(k in 1:i) temp <- temp + d[i+1-k, j] * wm[k]
         m_mat[i,j] <- temp
-        # Siguiente directa
+        # next direct expression
         if(j < jmax) d[i, j+1] <- m_mat[i,j] * p * 0.5 # modified dec-2025 temp * p
       }
     }
-    # Anuales
+    # Annuals
     for(j in 1:jmax) for(i in 1:H) {
       temp <- 0
       for(k in 1:i) temp <- temp + d[i+1-k, j] * wa[k]
       a_mat[i,j] <- temp
     }
-    # Finales
+    # Finals
     for(j in 1:jmax) for(i in 1:H) {
       temp <- 0
       for(k in 1:i) temp <- temp + d[i+1-k, j] * wf[k]
       e_mat[i,j] <- temp
     }
     
-    # ---- Sumatorias ----
+    # ---- summations ----
     sumd <- rowSums(d)
     suma <- rowSums(a_mat)
     summ <- rowSums(m_mat)
     sume <- rowSums(e_mat)
     
-    # ---- Valores económicos ----
+    # ---- Economic values ----
     vecd <- c((1:T)*ecd, rep(T*ecd, H-T))
     veca <- c((1:T)*eca, rep(T*eca, H-T))
     vecm <- c((1:T)*ecm, rep(T*ecm, H-T))
     vece <- c((1:T)*ece, rep(T*ece, H-T))
     
-    # ---- Matriz tempo ----
+    # ---- tempo matrix ----
     tempo <- matrix(0,H,H)
     for(i in 1:H) for(j in i:H) {
       idx <- j+1-i
@@ -142,7 +141,7 @@ server <- function(input, output, session) {
       }
     }
     
-    # ---- bE2 y sumbe2 ----
+    # ---- bE2 and sumbe2 ----
     n_progE2 <- u2
     bE2 <- matrix(0, H, H)
     for(i in 1:H) for(j in i:H) {
@@ -156,42 +155,41 @@ server <- function(input, output, session) {
       bE2[i, j] <- acc
     }
     sumbe2 <- sapply(1:H, function(j) sum(bE2[1:j, j]))
-    fr <- (1/(1+r))^(1:H)
+    fr <- (1/(1+r))^((1:H)-1) #modified 6/12/25
     IA2 <- sum(fr * sumbe2)
     
-    # ---- bE3 y sumbe3 ----
+    # ---- bE3 and sumbe3 ----
     n_progE3 <- u2
     bE3 <- matrix(0,H,H)
-    for(i in 1:H) {
-      if((i+2) <= H) {
-        for(j in (i+2):H) {
+    for(i in 1:(H-2)) {  # modified 6/12/25: H-2
+      for(j in (i+2):H) {  # modified 6/12/25: j starting i+2
+
           acc <- 0
           for(k in 1:min(i, n_progE3)) {
             ii <- i - k + 1; jj <- j - k - 1
-            if(ii >= 1 && jj >= 1 && ii <= H && jj <= H) {
+            if(ii >= 1 && jj >= 1) {
               acc <- acc + tempo[ii, jj] * progE3[k]
             }
           }
-          bE3[i, j] <- acc
+          bE3[i+2, j] <- acc #modified 6/12/25: starting i+2
         }
       }
-    }
     sumbe3 <- sapply(1:H, function(j) sum(bE3[1:j, j])/2)
     IA3 <- sum(fr * sumbe3)
     
-    # ---- Costos ----
+    # ---- Costs ----
     cosT1 <- rep(0, H); cosT1[1] <- costoa + costoi
-    if(T > 1) for(i in 2:T) cosT1[i] <- costoa * fr[i-1]
+    if(T > 1) for(i in 2:T) cosT1[i] <- costoa * fr[i] #modified 6/12/25
     CB1 <- sum(cosT1)
     cosT2 <- rep(0, H); cosT2[1] <- ram1 * c * b1
-    if(T > 1) for(i in 2:T) cosT2[i] <- ram1 * c * b1 * fr[i-1]
+    if(T > 1) for(i in 2:T) cosT2[i] <- ram1 * c * b1 * fr[i] #modified 6/12/25
     CB2 <- sum(cosT2)
     cosT3 <- rep(0, H)
-    for(i in 3:(T+2)) if(i <= H) cosT3[i] <- ram2 * noveE2 * b2 * fr[i-1]
+    for(i in 3:(T+2)) if(i <= H) cosT3[i] <- ram2 * noveE2 * b2 * fr[i] #modified 6/12/25
     CB3 <- sum(cosT3)
     costot <- CB1 + CB2 + CB3
     
-    # Resultados extendidos
+    # Extended results
     Benefit_stud   <- IA2 + IA3 - CB1
     Benefit_all    <- IA2 + IA3 - CB1 - CB2 - CB3
     Benefit_T1     <- CB2 - CB1
@@ -202,7 +200,7 @@ server <- function(input, output, session) {
     ICR_all        <- (IA2 + IA3) / (CB1 + CB2 + CB3)
     CIR            <- CB1 / (IA2 + IA3)
     
-    # agregar symbol column!
+    # add symbol column!
     values <- round(c(
       IA2/1000, IA3/1000, CB1/1000, CB2/1000, CB3/1000,
       Benefit_stud/1000, Benefit_all/1000, Benefit_T1/1000,
@@ -210,7 +208,6 @@ server <- function(input, output, session) {
       ROI, ICR_all, CIR*100
     ), 1)
     
-    # Convierte a carácter solo la última fila, agregando "%"
     values_char <- as.character(values)
     values_char[length(values_char)] <- paste0(values_char[length(values_char)], " %")
     
@@ -224,7 +221,7 @@ server <- function(input, output, session) {
         "Income to cost ratio (only stud costs)",
         "Income to cost ratio in T2",
         "Income to cost ratio in T3",
-        "Return to investment (stud costs)",
+        "Return to investment (ROI)",
         "Income to cost ratio (all costs)",
         "Cost to income ratio (CIR)"
       ),
@@ -246,13 +243,8 @@ server <- function(input, output, session) {
     
     colnames(tbl)[3] <- input$raza
     
-    # # ---- Tabla resultados ----
-    # tbl <- data.frame(
-    #   Parameter = c("IA2","IA3","CB1","CB2","CB3","Net Benefit"),
-    #   Value     = round(c(IA2, IA3, CB1, CB2, CB3, IA2+IA3-CB1),2)
-    # )
-    
-    # ---- Time series para gráfico ----
+ 
+    # ---- Time series to plot ----
     
     library(tidyr)
     library(ggplot2)
@@ -268,7 +260,7 @@ server <- function(input, output, session) {
     Benefit_in_T2T3       <- A2_an + A3_an - B1_an
     Benefit_in_T2T3_all   <- A2_an + A3_an - B1_an - B2_an - B3_an
     
-    # Usar nombres simples sin signos
+    # simple names without sign
     df_time <- data.frame(
       Year      = 0:H,
       T2        = Benefit_in_T2,
@@ -276,7 +268,6 @@ server <- function(input, output, session) {
       T2T3_all  = Benefit_in_T2T3_all
     )
     
-    # Convertir a formato largo
     df_long <- df_time %>%
       pivot_longer(-Year, names_to = "Scenario", values_to = "Benefit") %>%
       mutate(Scenario = factor(
@@ -301,14 +292,13 @@ server <- function(input, output, session) {
       theme_minimal() +
       theme(legend.position = "top")
     
-    # Lo retornás como siempre:
     list(table = tbl, plot = plot_obj, data = df_time)
     
   }
   
   observeEvent(input$go, {
     params <- reactiveValuesToList(input)[c(
-      "c","b1","b2","L","f","p","m1","m2","u1","u2",
+      "c","b1","b2","f","p","m1","m2","u1","u2",
       "ecd","eca","ecm","ece","costoi","costoa","ram1","ram2","r","T","H"
     )]
     res <- do.call(calcular, params)
